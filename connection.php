5ad7fbaef5809daebe50a0c067bd8b5d3e4f91e6
ishwar2303 <68193554+ishwar2303@users.noreply.github.com>
2021-03-10 12:39:25 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,4 @@
 <?php 
-    $host_name = 'database-1.c3mppahrvey4.ap-south-1.rds.amazonaws.com';
-    $user_name = 'ishwar';
-    $password = 'qwerty1234';
-    $db_name = 'ctf';
-
     
     $host_name = '127.0.0.1';
     $user_name = 'root';
